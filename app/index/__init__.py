0b7a3d0bee279bfa716830c6366d5ff9602607ae
Ronald Kemei <kemeirnld@gmail.com>
2024-06-02 13:45:45 +0300

    Add book module and index

---
+++ app/index/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask, Blueprint, jsonify
+
+index_bp = Blueprint('index_bp', __name__,)
+
+
+@index_bp.route('/', methods = ['GET'])
+def index():
+    return '<h1> Welcome to Library Management System </h1>'
